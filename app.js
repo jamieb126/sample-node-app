8886c0e29c46d884497f3ad64e1b679e215c760a
jamieb126 <135164464+jamieb126@users.noreply.github.com>
2024-07-29 16:04:43 +0100

    Changed greeting

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
-var express     = require('express');
-var morgan      = require('morgan');
+var express = require("express");
+var morgan = require("morgan");
 
 var app = express();
-var server = require('http').Server(app);
+var server = require("http").Server(app);
 port = process.env.PORT || 8080;
 
-app.get('/', (req, res) => {
-    res.send("Hello World!!! Node Sample App is up and running!");
+app.get("/", (req, res) => {
+  res.send("Hi there, hello, welcome to my panda inspired website!");
 });
 
-app.get('/ping', (req, res) => {
-    res.send("Pong!");
+app.get("/ping", (req, res) => {
+  res.send("Pong!");
 });
 
-app.post('/ping', (req, res) => {
-    res.send("POST: Pong!");
+app.post("/ping", (req, res) => {
+  res.send("POST: Pong!");
 });
 
 // use morgan to log requests to the console
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
 server.listen(port);
-console.log('App running at http://localhost:' + port);
+console.log("App running at http://localhost:" + port);
